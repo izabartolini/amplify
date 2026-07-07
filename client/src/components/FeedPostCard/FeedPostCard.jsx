35f8d87c6c6166c772c67c2e49b6ee3ad0189e0d
Juliana <julicarvalhos24@gmail.com>
2026-07-06 21:04:35 -0300

    feat: clicking username in Feed navigates to user profile closes #112

--- client/src/components/FeedPostCard/FeedPostCard.jsx
+++ client/src/components/FeedPostCard/FeedPostCard.jsx
@@ -1,4 +1,5 @@
 import './FeedPostCard.css'
+import { Link } from 'react-router-dom'
 
 function FeedPostCard({ post }) {
   const avatarUrl = post.user?.profile_picture
@@ -7,15 +8,17 @@ function FeedPostCard({ post }) {
   return (
     <div className="feed-post-card">
       <div className="feed-post-header">
-        <img src={avatarUrl} alt={post.user?.name} className="feed-post-avatar" />
-        <div className="feed-post-author">
-          <span className="feed-post-name">{post.user?.username}</span>
-          <span className="feed-post-location">
-            {post.user?.city && post.user?.state
-              ? `${post.user.city}, ${post.user.state}`
-              : 'Localização não informada'}
-          </span>
-        </div>
+        <Link to={`/profile/${post.user?.id}`} className="feed-post-author-link">
+          <img src={avatarUrl} alt={post.user?.name} className="feed-post-avatar" />
+          <div className="feed-post-author">
+            <span className="feed-post-name">{post.user?.username}</span>
+            <span className="feed-post-location">
+              {post.user?.city && post.user?.state
+                ? `${post.user.city}, ${post.user.state}`
+                : 'Localização não informada'}
+            </span>
+          </div>
+        </Link>
       </div>
 
       {post.medias && post.medias.length > 0 && (
